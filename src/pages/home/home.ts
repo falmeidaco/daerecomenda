4721cd0ff0183ffe04092edc1e68712a6a816c93
Antonio Felipe de Almeida Costa <felipe.costa@fortbrasil.com.br>
2018-11-20 12:58:52 -0300

    correção saudação

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,24 +11,22 @@ export class HomePage {
   period: String;
   
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    //Set day saudation
+    let time_hour:number = new Date().getHours();
+    if (time_hour >= 0 && time_hour < 12) {
+      this.period = 'bom dia';
+    } else if (time_hour >= 12 && time_hour < 18) {
+      this.period = 'boa tarde';
+    } else {
+      this.period = 'boa noite' + time_hour;
+    }
   }
 
   ngOnInit() {
-    //Set day saudation
-    let time_hour:number = new Date().getHours();
-    if (time_hour > 0 && time_hour < 12) {
-      this.period = 'bom dia';
-    } else if (time_hour > 12 && time_hour < 18) {
-      this.period = 'boa tarde';
-    } else {
-      this.period = 'boa noite';
-    }
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad HomePage');
   }
-
-
 
 }
